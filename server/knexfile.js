265b1b0ec51de099a72bf24f774fc5f3e4345d35
ctaggart27 <ctaggart27@gmail.com>
2017-06-09 14:13:23 -0600

    oops

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,11 +9,10 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/classifieds_test'
-  }
-// ,
+  },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
